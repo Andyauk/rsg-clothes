b3693a19bc672a247c0eeb4dd863eec231ae6b46
RexshackGaming <rexshackuk@gmail.com>
2023-09-30 15:58:25 +0100

    version checker rework

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,17 +3,20 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 game 'rdr3'
 
 description 'rsg-clothes'
-version '1.0.0'
+version '1.0.1'
+
+shared_scripts {
+    'config.lua'
+}
 
 client_scripts {
     'utils/clotheslist.lua',
     'utils/functions.lua',
-    'client/client.lua',
-    'config.lua',
+    'client/client.lua'
 }
 
 server_scripts {
-    'config.lua',   
+    'server/versionchecker.lua',
     'server/server.lua',
     '@oxmysql/lib/MySQL.lua',
 }
